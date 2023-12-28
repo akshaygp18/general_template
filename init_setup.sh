4bf1a43e4a5eabccd9323bc0ddc39b273bbf5fca
akshaygp18 <akshaygp41@gmail.com>
2023-12-28 13:11:17 +0530

    torch updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,13 +1,13 @@
 echo [$(date)]: "START"
-export _VERSION_=3.7
+export _VERSION_=3.8
 echo [$(date)]: "creating environment with python ${_VERSION_}"
 conda create --prefix ./env python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
-echo [$(date)]: "installing pytorch 1.8.1"
-pip install torch==2.0.0 --index-url https://download.pytorch.org/whl/cu118
+echo [$(date)]: "installing pytorch 2.0.0"
+conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 pytorch-cuda=11.8 -c pytorch -c nvidia
 echo [$(date)]: "END"
 # to remove everything -
 # rm -rf env/ .gitignore conda.yaml README.md .git/
